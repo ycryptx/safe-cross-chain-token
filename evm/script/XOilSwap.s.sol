11186b05054bb117136dfdad378401f95c6a54d4
ycryptx <yonatan.med@gmail.com>
2023-06-21 17:51:28 +0900

    ethereum side working on local testnet ⚡

--- evm/script/XOilSwap.s.sol
+++ evm/script/XOilSwap.s.sol
@@ -2,16 +2,23 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "src/XOilSwap.sol";
+import "../src/XOilSwap.sol";
 
 contract XOilSwapScript is Script {
-    function setUp() public {}
+    address private xOilTokenAddress;
+    address private nativeTokenAddress;
+    XOilSwap private xOilSwap;
+
+    function setUp() public {
+        xOilTokenAddress = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512; // TODO: change to actual xOIL token addres
+        nativeTokenAddress = 0x5FbDB2315678afecb367f032d93F642f64180aa3; // TODO: change to actual native token addres
+    }
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // NFT nft = new NFT("NFT_tutorial", "TUT", "baseUri");
+        xOilSwap = new XOilSwap(xOilTokenAddress, nativeTokenAddress);
 
         vm.stopBroadcast();
     }
--- evm/test/XOilSwap.sol
+++ evm/test/XOilSwap.sol
@@ -3,23 +3,82 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/XOilSwap.sol";
+import "../src/CustomMintableToken.sol";
 
 contract OilSwapTest is Test {
     XOilSwap private xOilSwap;
-    address private xOilTokenAddress;
+    CustomMintableToken private xOilContract;
+    CustomMintableToken private IscContract;
+    address private alice;
 
     function setUp() public {
-        xOilTokenAddress = 0x6d2e03b7EfFEae98BD302A9F836D0d6Ab0002766;
+        alice = 0x1f9090aaE28b8a3dCeaDf281B0F12828e676c326;
+        xOilContract = new CustomMintableToken(1000, "Wormhole xOil", "xOIL");
+        IscContract = new CustomMintableToken(
+            1000,
+            "International Stable Currency",
+            "ISC"
+        );
+
+        // init swap contract
+        xOilSwap = new XOilSwap(address(xOilContract), address(IscContract));
+
+        // fund a user with some xOil
+        xOilContract.transfer(address(alice), 100);
+
+        // fund the swap contract with native token
+        IscContract.transfer(address(xOilSwap), 50);
     }
 
-    function testConstructor() public {
-        xOilSwap = new XOilSwap(xOilTokenAddress);
-        assertEq(xOilSwap.xOIL(), xOilTokenAddress);
+    function testNotEnoughXOilAllowance() public {
+        vm.startPrank(address(alice));
+        vm.expectRevert("xOilSwap: amount exceeds allowance");
+        xOilSwap.swap(100);
+        vm.stopPrank();
+
+        // sanity check that no balances changed
+        assertEq(xOilContract.balanceOf(address(alice)), 100);
+        assertEq(IscContract.balanceOf(address(xOilSwap)), 50);
     }
 
-    function testSwap() public {
-        xOilSwap = new XOilSwap(xOilTokenAddress);
-        uint256 result = xOilSwap.swap(100);
-        assertEq(result, 100);
+    function testNotEnoughNativeTokenBalance() public {
+        vm.startPrank(address(alice));
+        xOilContract.approve(address(xOilSwap), 100);
+        vm.expectRevert("xOilSwap: amount exceeds native token balance");
+        xOilSwap.swap(100);
+        vm.stopPrank();
+
+        // sanity check that no balances changed
+        assertEq(xOilContract.balanceOf(address(alice)), 100);
+        assertEq(IscContract.balanceOf(address(xOilSwap)), 50);
+    }
+
+    function testSuccessfulSwap() public {
+        assertEq(IscContract.balanceOf(address(alice)), 0);
+        vm.startPrank(address(alice));
+        xOilContract.approve(address(xOilSwap), 100);
+        xOilSwap.swap(45);
+        vm.stopPrank();
+
+        assertEq(xOilContract.balanceOf(address(alice)), 55);
+        assertEq(IscContract.balanceOf(address(alice)), 45);
+        assertEq(IscContract.balanceOf(address(xOilSwap)), 5);
+    }
+
+    function testSwapDrainsContractBalance() public {
+        assertEq(IscContract.balanceOf(address(alice)), 0);
+        vm.startPrank(address(alice));
+        xOilContract.approve(address(xOilSwap), 100);
+        xOilSwap.swap(50);
+        vm.stopPrank();
+
+        assertEq(xOilContract.balanceOf(address(alice)), 50);
+        assertEq(IscContract.balanceOf(address(alice)), 50);
+        assertEq(IscContract.balanceOf(address(xOilSwap)), 0);
+
+        vm.startPrank(address(alice));
+        vm.expectRevert("xOilSwap: amount exceeds native token balance");
+        xOilSwap.swap(1);
+        vm.stopPrank();
     }
 }
